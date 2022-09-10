2344bc330be059a0e9c5e08be5be9569ea5ab7b7
jaeyunha <jaeyunha@berkeley.edu>
2022-09-10 01:12:23 -0700

    after completing todo app

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,12 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Learn Basic React',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'Styling Components',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'Make To-do App',
+      checked: false,
+    },
+  ]);
+  const nextId = useRef(4);
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 }
--- todo-app/src/components/TodoInsert.jsx
+++ todo-app/src/components/TodoInsert.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
     <div>
-      <form className="TodoInsert">
-        <input placeholder="Enter To-do" />
+      <form className="TodoInsert" onSubmit={onSubmit}>
+        <input placeholder="Enter To-do" value={value} onChange={onChange} />
         <button type="submit">
           <MdAdd />
         </button>
